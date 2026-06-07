68691c544807e78711b833e279b301202337910e
Prantik <30826501+prantikdatta@users.noreply.github.com>
2026-06-07 12:37:43 +0530

    Add SQL solution for LeetCode 610 Triangle Judgement

---
+++ Leet Top 50/Easy/610_Triangle_Judgement/solution.sql
@@ -0,0 +1,12 @@
+-- LeetCode 610. Triangle Judgement
+
+
+select *,
+    case
+        when x+y > z 
+        and x+z > y
+        and y+z> x
+        then 'Yes'
+        else 'No'
+        end as triangle
+    from triangle
