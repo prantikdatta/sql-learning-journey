a36f785cefd1ef24238f2826d77772efaae5cb1c
Prantik <30826501+prantikdatta@users.noreply.github.com>
2026-06-11 12:49:36 +0530

    Create solution.sql

---
+++ Leet Top 50/Medium/0180-consecutive-numbers/solution.sql
@@ -0,0 +1,13 @@
+```sql
+SELECT DISTINCT
+    num AS ConsecutiveNums
+FROM (
+    SELECT
+        num,
+        LAG(num, 1) OVER (ORDER BY id) AS prev_num_1,
+        LAG(num, 2) OVER (ORDER BY id) AS prev_num_2
+    FROM Logs
+) l
+WHERE num = prev_num_1
+  AND num = prev_num_2;
+```
